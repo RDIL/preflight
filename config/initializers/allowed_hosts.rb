f18a04a28a697b016fc7e22aa3bea2c138933581
Reece Dunham <me@rdil.rocks>
2025-12-05 20:59:21 -0500

    Fix DJ

--- config/initializers/allowed_hosts.rb
+++ config/initializers/allowed_hosts.rb
@@ -1,7 +1,5 @@
-if Rails.env.development?
-  Rails.application.config.hosts << ENV.fetch('CANONICAL_DOMAIN')
+Rails.application.config.hosts << ENV.fetch('CANONICAL_DOMAIN', 'fall.back')
 
-  DelayedJobWeb.class_eval do
-    set :host_authorization, :allowed_hosts => Rails.application.config.hosts
-  end
+DelayedJobWeb.class_eval do
+  set :host_authorization, :allowed_hosts => Rails.application.config.hosts
 end
