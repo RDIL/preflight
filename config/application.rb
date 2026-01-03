48265a95fa061a31949c032167595b44ce7cb551
Reece Dunham <me@rdil.rocks>
2026-01-03 16:12:33 -0500

    Gem cleanup

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
 require 'delegate'
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,5 +31,8 @@ module Preflight
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
